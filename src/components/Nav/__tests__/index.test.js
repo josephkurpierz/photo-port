62fc7780f9f39ec80ccf1f51c3188971a9f55f08
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-09-10 21:46:38 -0500

    photolist and tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,16 +3,30 @@ import { render, cleanup, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+  {name:'portraits', description:'Portraits of people in my life'}
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
   //baseline test
   it('renders', () => {
-    render(<Nav/>);
+    render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    />);
   });
   // snapshot test
   it('matches snapshot', () => {
-    const { asFragment } = render(<Nav/>);
+    const { asFragment } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      />);
     //assert value comparison
     expect(asFragment()).toMatchSnapshot();
   });
@@ -21,7 +35,11 @@ describe('Nav component', () => {
 describe('emoki is visible', () => {
   it('inserts emoji into the h2', () => {
     //arrange
-    render(<Nav/>);
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      />);
     //assert
     expect(screen.getByLabelText('camera')).toHaveTextContent('📸');
   })
@@ -30,7 +48,11 @@ describe('emoki is visible', () => {
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     //arrange
-    render(<Nav/>);
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      />);
     //assert
     expect(screen.getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(screen.getByTestId('about')).toHaveTextContent('About me');
